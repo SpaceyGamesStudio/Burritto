98d13e24b11260c945dae3d470c44ac35c3751fd
michalolejko <michal.olejko2@gmail.com>
2025-03-04 22:32:50 +0100

    chodzenie wzgledem kursora myszy - pojebana akcja

--- Player/player.gd
+++ Player/player.gd
@@ -1,33 +1,21 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
+@onready var model: Node3D = $Model
+@export var SPEED: float = 100.0
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	
-	movement()
+	movement(delta)
 	move_and_slide()
 
-
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("jump"):
-		velocity.y = JUMP_VELOCITY
-
-
-func movement() -> void:
+func movement(delta: float) -> void:
 	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	var move_direction := Vector3.ZERO
+	var y_2d := -model.global_transform.basis.z
+	var x_2d := model.global_transform.basis.x
+	
+	move_direction -= input_dir.y * y_2d
+	move_direction += input_dir.x * x_2d
+
+	move_direction = move_direction.normalized()
+	velocity = move_direction * SPEED * delta
+	move_and_slide()
